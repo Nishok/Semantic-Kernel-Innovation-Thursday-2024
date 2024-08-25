eee1d4f26e545800e43b7f81a3d4be2df8afd6b5
Björn Truyts <bjorn.truyts@tillit.be>
2024-08-26 00:25:31 +0200

    Added the project showcasing functions and plugins, giving the kernel access to your own code.

---
+++ 3-CustomDatePlugin/Plugins/TimePlugin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace CustomDatePlugin.Plugins
+{
+    public class TimePlugin
+    {
+        [KernelFunction]
+        [Description("Get the current date and time in UTC.")]
+        public string GetUTCDateTime()
+        {
+            return DateTime.UtcNow.ToString("R");
+        }
+
+        [KernelFunction]
+        [Description("Get the current date and time in local timezone.")]
+        public string GetTimeZone()
+        {
+            return DateTime.Now.ToString("R");
+        }
+    }
+}
---
+++ 3-CustomDatePlugin/Program.cs
@@ -0,0 +1,70 @@
+﻿using CustomDatePlugin.Config;
+using CustomDatePlugin.Plugins;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+#region Builder + Configuration Prep
+
+string configSection = "AzureOpenAI";
+
+IConfigurationRoot config = new ConfigurationBuilder()
+                                .AddUserSecrets<Program>()
+                                .Build();
+
+IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+
+kernelBuilder.Services.TryAddSingleton<IConfiguration>(config);
+kernelBuilder.Services.AddOptions<OpenAIConfig>().BindConfiguration(configSection).ValidateOnStart();
+
+var configValues = config.GetSection(configSection).Get<OpenAIConfig>()!;
+
+kernelBuilder.AddAzureOpenAIChatCompletion(configValues.ModelId, configValues.Endpoint, configValues.ApiKey);
+
+#endregion
+
+//Add the Time Plugin to the kernel
+kernelBuilder.Plugins.AddFromType<TimePlugin>();
+
+Kernel kernel = kernelBuilder.Build();
+
+//Allow the kernel to automatically invoke kernel functions whenever it sees fit
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+#region Chat Completion
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory();
+
+while(true)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("User > ");
+
+    history.AddUserMessage(Console.ReadLine() ?? "");
+
+    //Make sure to pass the openAIPromptExecutionSettings and kernel, so it knows about the kernel functions
+    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(history, openAIPromptExecutionSettings, kernel);
+    string fullResponse = "";
+
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.Write("AI > ");
+    await foreach (var item in result)
+    {
+        Console.Write(item.Content);
+        fullResponse += item.Content;
+    }
+
+    history.AddAssistantMessage(fullResponse);
+
+    Console.WriteLine();
+}
+
+#endregion
